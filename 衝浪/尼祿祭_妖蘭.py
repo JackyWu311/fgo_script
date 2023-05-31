4d27b6453c3c274cc46c0f5c178da39016313a6e
JackyWu311 <apple970066@gmail.com>
2023-05-31 14:44:25 +0800

    Update 尼祿祭_妖蘭.py

--- 衝浪/尼祿祭_妖蘭.py
+++ 衝浪/尼祿祭_妖蘭.py
@@ -134,7 +134,7 @@ while True:
         touch_sleep(switch4,0.7)
         touch_sleep(switch_confirm,3.5,True)
         touch_sleep(ms,0.7)
-        touch_sleep(ms1,0.7)
+        touch_sleep(ms1,2,True)
         
         touch_sleep(sk2_1,2,True)
         touch_sleep(sk2_2,0.7)
